b873080998c5c4bd46a24878213d0cc9783079c2
Jussi Vatjus-Anttila <jussi.vatjus-anttila@arm.com>
2016-04-20 10:33:20 +0300

    update mbed trace dependency

--- test/Test.cpp
+++ test/Test.cpp
@@ -3,9 +3,9 @@
  */
 
 /**
- * \file \test_libTrace\Test.c
+ * \file \test\Test.c
  *
- * \brief Unit tests for libTrace
+ * \brief Unit tests for mbed-client-cli
  */
 #include <string.h>
 #include <stdlib.h>
@@ -18,8 +18,8 @@
 #include "mbed-cpputest/CppUTest/SimpleString.h"
 #include "mbed-cpputest/CppUTest/CommandLineTestRunner.h"
 
-#define YOTTA_CFG_MTRACE
-#define YOTTA_CFG_MTRACE_FEA_IPV6 0
+#define YOTTA_CFG_MBED_TRACE
+#define YOTTA_CFG_MBED_TRACE_FEA_IPV6 0
 #include "mbed-trace/mbed_trace.h"
 #include "mbed-client-cli/ns_cmdline.h"
 #define MAX(x,y)   (x>y?x:y)
