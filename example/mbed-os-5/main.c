385c44a1bca256d09eac5196b0272a029f70f5d4
Jussi Vatjus-Anttila <jussiva@gmail.com>
2018-07-14 16:39:46 +0300

    add mutex to example

--- example/mbed-os-5/main.c
+++ example/mbed-os-5/main.c
@@ -14,20 +14,50 @@
  * limitations under the License.
  */
 #include <stdio.h>
+#include <string>
 
 #include "mbed.h"
-#include "mbed_trace.h"
+#include "mbed-trace/mbed_trace.h"
+#include "mbed-client-cli/ns_cmdline.h"
 
 #define TRACE_GROUP                 "main"
 
-static int cmd_dummy(int argc, char *argv[]);
+static Mutex SerialOutMutex;
+void serial_out_mutex_wait()
+{
+    SerialOutMutex.lock();
+}
+void serial_out_mutex_release()
+{
+    osStatus s = SerialOutMutex.unlock();
+    MBED_ASSERT(s == osOK);
+}
 
+// dummy command with some option
+static int cmd_dummy(int argc, char *argv[]) {
+  if( cmd_has_option(argc, argv, "o") ) {
+    cmd_printf("This is o option");
+  } else {
+        return CMDLINE_RETCODE_INVALID_PARAMETERS;
+  }
+  return CMDLINE_RETCODE_SUCCESS;
+}
 int main(void)
 {
     // Initialize trace library
     mbed_trace_init();
-    cmd_init( &myprint );              // initialize cmdline with print function
-    cmd_set_ready_cb( cmd_ready_cb );  // configure ready cb
+    // Register callback used to lock serial out mutex
+    mbed_trace_mutex_wait_function_set( serial_out_mutex_wait );
+    // Register callback used to release serial out mutex
+    mbed_trace_mutex_release_function_set( serial_out_mutex_release );
+
+    // Initialize cmd library
+    cmd_init( 0 );
+    // Register callback used to lock serial out mutex
+    cmd_mutex_wait_func( serial_out_mutex_wait );
+    // Register callback used to release serial out mutex
+    cmd_mutex_release_func( serial_out_mutex_release );
+    // add dummy -command
     cmd_add("dummy", cmd_dummy,
       "dummy command",
       "This is dummy command, which does not do anything except\n"
@@ -40,16 +70,4 @@ int main(void)
             cmd_char_input(c);
         }
     }
-}
-
-
-
-// dummy command with some option
-static int cmd_dummy(int argc, char *argv[]) {
-  if( cmd_has_option(argc, argv, "o") ) {
-    cmd_printf("This is o option");
-  } else {
-        return CMDLINE_RETCODE_INVALID_PARAMETERS;
-  }
-  return CMDLINE_RETCODE_SUCCESS;
 }
